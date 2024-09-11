40f97e5f0b18765d62e0209f3b87186266bce51d
Yassin Elsir <elsiryas@hu-berlin.de>
2024-09-11 11:06:54 +0200

    created idqn agent

--- src/agents/q/dqn_agent.py
+++ src/agents/q/dqn_agent.py
@@ -1,40 +1,28 @@
 import numpy as np
 import torch
-from torch import optim, nn
-from torchrl.data import ReplayBuffer, ListStorage
-
-from src.networks.deep_q_network import DeepQNetwork
+from torch import nn
 
 
 class DqnAgent:
-    def __init__(self, state_dim, hidden_dim, hidden_output_dim, n_actions, learning_rate, epsilon, gamma,
-                 buffer_capacity, batch_size):
-        self.q_network = DeepQNetwork(state_dim, hidden_dim, hidden_output_dim, n_actions)
-        self.target_network = DeepQNetwork(state_dim, hidden_dim, hidden_output_dim, n_actions)
-        self.target_network.load_state_dict(self.q_network.state_dict())
-        self.optimizer = optim.Adam(self.q_network.parameters(), lr=learning_rate)
-        self.replay_buffer = ReplayBuffer(batch_size=buffer_capacity, storage=ListStorage(max_size=buffer_capacity))
+    def __init__(self, q_network, target_q_network, optimizer, replay_buffer, action_dim, epsilon, gamma):
+        self.q_network = q_network
+        self.target_network = target_q_network
+        self.optimizer = optimizer
+        self.replay_buffer = replay_buffer
+
+        self.action_dim = action_dim
         self.epsilon = epsilon
         self.gamma = gamma
-        self.state_dim = state_dim
-        self.n_actions = n_actions
-        self.batch_size = batch_size
 
     def select_action(self, state):
         if np.random.random() < self.epsilon:
-            return np.random.randint(self.n_actions)
+            return np.random.randint(self.action_dim)
         else:
             with torch.no_grad():
                 if not isinstance(state, np.ndarray):
                     state = np.array(state)
 
-                if state.ndim == 1:
-                    state = state.reshape(1, -1)
-                elif state.ndim > 2:
-                    raise ValueError(f"State has too many dimensions: {state.shape}")
-
-                if state.shape[1] != self.state_dim:
-                    raise ValueError(f"State has {state.shape[1]} features, expected {self.state_dim}")
+                state = state.reshape(1, -1)
 
                 state = torch.FloatTensor(state)
 
@@ -42,55 +30,33 @@ class DqnAgent:
                 return action_q_values.argmax().item()
 
     def update(self):
-        if len(self.replay_buffer) < self.batch_size:
-            return
+        if self.replay_buffer.can_sample():
 
-        states, actions, rewards, next_states, dones = self.get_batch()
+            states, actions, rewards, next_states, dones = self.get_batch()
 
-        action_q_values, _ = self.q_network(states)
-        next_action_q_values, _ = self.target_network(next_states)
+            action_q_values, _ = self.q_network(states)
+            next_action_q_values, _ = self.target_network(next_states)
 
-        q_value = action_q_values.gather(1, actions.unsqueeze(1)).squeeze(1)
-        next_q_value = next_action_q_values.max(1)[0]
-        expected_q_value = rewards + self.gamma * next_q_value * (1 - dones)
+            q_value = action_q_values.gather(1, actions.unsqueeze(1)).squeeze(1)
+            next_q_value = next_action_q_values.max(1)[0]
+            expected_q_value = rewards + self.gamma * next_q_value * (1 - dones)
 
 
-        loss = nn.MSELoss()(q_value, expected_q_value.detach())
+            loss = nn.MSELoss()(q_value, expected_q_value.detach())
 
-        self.optimizer.zero_grad()
-        loss.backward()
+            self.optimizer.zero_grad()
+            loss.backward()
 
-        self.optimizer.step()
+            self.optimizer.step()
 
     def get_batch(self):
-        states, actions, rewards, next_states, dones = self.replay_buffer.sample()
+        batch = self.replay_buffer.sample()
+        states, actions, rewards, next_states, dones  = zip(*batch)
 
-        states = np.array(states)
-        states = torch.FloatTensor(states)
-
-        actions = np.array(actions)
-        actions = torch.LongTensor(actions)
-
-        rewards = np.array(rewards)
-        rewards = torch.FloatTensor(rewards)
-
-        next_states = np.array(next_states)
-        next_states = torch.FloatTensor(next_states)
-
-        dones = np.array(dones)
-        dones = torch.FloatTensor(dones)
-
-        return states, actions, rewards, next_states, dones
-
-    def add_to_buffer(self, data):
-        self.replay_buffer.add(data)
-
-    def step(self, env, state):
-
-        action = self.select_action(state)
-
-        next_state, reward, done, _, _ = env.step(action)
-
-        self.add_to_buffer((state, action, reward, next_state, done))
-
-        return next_state, reward, done
+        return (
+            torch.stack(states),
+            torch.stack(actions),
+            torch.stack(rewards),
+            torch.stack(next_states),
+            torch.stack(dones)
+        )
--- src/agents/q/idqn_agent.py
+++ src/agents/q/idqn_agent.py
@@ -1,96 +1,92 @@
 import numpy as np
 import torch
 from torch import optim, nn
-from torchrl.data import ReplayBuffer, ListStorage
-
+from src.agents.q.dqn_agent import DqnAgent
+from src.common.replay_buffer import ReplayBuffer
 from src.networks.deep_q_network import DeepQNetwork
 
 
 class IdqnAgent:
-    def __init__(self, state_dim, hidden_dim, hidden_output_dim, n_actions, learning_rate, epsilon, gamma,
+    def __init__(self, n_agents, state_dim, hidden_dim, hidden_output_dim, action_dim, learning_rate, epsilon, gamma,
                  buffer_capacity, batch_size):
-        self.q_network = DeepQNetwork(state_dim, hidden_dim, hidden_output_dim, n_actions)
-        self.target_network = DeepQNetwork(state_dim, hidden_dim, hidden_output_dim, n_actions)
-        self.target_network.load_state_dict(self.q_network.state_dict())
-        self.optimizer = optim.Adam(self.q_network.parameters(), lr=learning_rate)
-        self.replay_buffer = ReplayBuffer(batch_size=buffer_capacity, storage=ListStorage(max_size=buffer_capacity))
+        self.agents = []
+        for _ in range(n_agents):
+            q_network = DeepQNetwork(state_dim, hidden_dim, hidden_output_dim, action_dim)
+            target_q_network = DeepQNetwork(state_dim, hidden_dim, hidden_output_dim, action_dim)
+            target_q_network.load_state_dict(q_network.state_dict())
+            optimizer = optim.Adam(q_network.parameters(), lr=learning_rate)
+            replay_buffer = ReplayBuffer(batch_size=batch_size, buffer_size=buffer_capacity)
+            agent = DqnAgent(
+                q_network=q_network,
+                target_q_network=target_q_network,
+                optimizer=optimizer,
+                replay_buffer=replay_buffer,
+                epsilon=epsilon,
+                gamma=gamma,
+                action_dim=action_dim
+            )
+            self.agents.append(agent)
+
+        self.replay_buffer = ReplayBuffer(batch_size=batch_size, buffer_size=buffer_capacity)
+
         self.epsilon = epsilon
         self.gamma = gamma
         self.state_dim = state_dim
-        self.n_actions = n_actions
+        self.action_dim = action_dim
         self.batch_size = batch_size
 
-    def select_action(self, state):
-        if np.random.random() < self.epsilon:
-            return np.random.randint(self.n_actions)
-        else:
-            with torch.no_grad():
-                if not isinstance(state, np.ndarray):
-                    state = np.array(state)
-
-                if state.ndim == 1:
-                    state = state.reshape(1, -1)
-                elif state.ndim > 2:
-                    raise ValueError(f"State has too many dimensions: {state.shape}")
-
-                if state.shape[1] != self.state_dim:
-                    raise ValueError(f"State has {state.shape[1]} features, expected {self.state_dim}")
-
-                state = torch.FloatTensor(state)
-
-                action_q_values, _ = self.q_network(state)
-                return action_q_values.argmax().item()
-
     def update(self):
-        if len(self.replay_buffer) < self.batch_size:
-            return
-
-        states, actions, rewards, next_states, dones = self.get_batch()
-
-        action_q_values, _ = self.q_network(states)
-        next_action_q_values, _ = self.target_network(next_states)
-
-        q_value = action_q_values.gather(1, actions.unsqueeze(1)).squeeze(1)
-        next_q_value = next_action_q_values.max(1)[0]
-        expected_q_value = rewards + self.gamma * next_q_value * (1 - dones)
+        for idx, agent in enumerate(self.agents):
+            agent.update()
 
 
-        loss = nn.MSELoss()(q_value, expected_q_value.detach())
+    def step(self, env):
+        states = []
+        next_states = []
+        rewards = []
+        dones = []
+        for idx, agent_id in enumerate(env.agents):
+            observation, reward, termination, truncation, _ = env.last()
+            obs_tensor = torch.FloatTensor(observation)
 
-        self.optimizer.zero_grad()
-        loss.backward()
+            if termination or truncation:
+                return rewards, [True]
+            else:
+                action = self.agents[idx].select_action(state=obs_tensor)
 
-        self.optimizer.step()
+            env.step(action)
+            next_observation = env.observe(agent_id)
 
-    def get_batch(self):
-        states, actions, rewards, next_states, dones = self.replay_buffer.sample()
+            next_obs_tensor = torch.FloatTensor(next_observation)
+            done_tensor = torch.BoolTensor([termination or truncation])
+            reward_tensor = torch.FloatTensor([reward])
 
-        states = np.array(states)
-        states = torch.FloatTensor(states)
+            states.append(obs_tensor)
+            next_states.append(next_obs_tensor)
+            rewards.append(reward_tensor)
+            dones.append(done_tensor)
 
-        actions = np.array(actions)
-        actions = torch.LongTensor(actions)
+            experience = (
+                obs_tensor,
+                next_obs_tensor,
+                reward_tensor,
+                done_tensor
+            )
 
-        rewards = np.array(rewards)
-        rewards = torch.FloatTensor(rewards)
+            self.agents[idx].replay_buffer.add(experience)
 
-        next_states = np.array(next_states)
-        next_states = torch.FloatTensor(next_states)
+        states = torch.stack(states)
+        next_states = torch.stack(next_states)
+        rewards = torch.tensor(rewards)
+        dones = torch.tensor(dones)
 
-        dones = np.array(dones)
-        dones = torch.FloatTensor(dones)
+        experience = (
+            states,
+            next_states,
+            rewards,
+            dones
+        )
 
-        return states, actions, rewards, next_states, dones
+        self.replay_buffer.add(experience)
 
-    def add_to_buffer(self, data):
-        self.replay_buffer.add(data)
-
-    def step(self, env, state):
-
-        action = self.select_action(state)
-
-        next_state, reward, done, _, _ = env.step(action)
-
-        self.add_to_buffer((state, action, reward, next_state, done))
-
-        return next_state, reward, done
+        return rewards, dones
